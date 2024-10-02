ff430c924952a2dddfaa2901fa3d480e2d7264d9
KiraYam <loicding93500@hotmail.com>
2024-10-02 15:41:22 +0200

    [add] simple player controller

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float h;
+    private float v;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveDirection = transform.forward * v + transform.right * h;
+
+        rb.AddForce(moveDirection * 10f);
+    }
+}
