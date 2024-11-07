7ad1fd4da401ddee0eef142ba43c535503bc212d
MaitreJarek <124875159+MaitreJarek@users.noreply.github.com>
2024-11-08 00:01:29 +0100

    Début Scene Test + fix on Toggle Scene + Fix on Move plateforme

--- Assets/Scenes/S_MovingPlatform.cs
+++ Assets/Scenes/S_MovingPlatform.cs
@@ -9,25 +9,37 @@ public class S_MovingPlatform : MonoBehaviour
     public float vitesse = 2f; // Vitesse du déplacement
     private float t = 0f; // Paramètre de progression (de 0 à 1)
 
+    private bool StopForASec = true;
+
     private void Update()
     {
-        // Lerp entre le point A et B en fonction de t
-        t += Time.deltaTime * vitesse; // Incrementation de t selon le temps écoulé
-        if (t > 1f) t = 1f; // Limite t à 1 pour ne pas dépasser la destination
-
-        // Déplacer la plateforme
-        transform.position = Vector3.Lerp(pointA, pointB, t);
-
-        // Revenir au point A une fois arrivé à B
-        if (t >= 1f)
+        if(StopForASec)
         {
-            // Inverser les points pour faire revenir la plateforme au point A
-            Vector3 temp = pointA;
-            pointA = pointB;
-            pointB = temp;
+            // Lerp entre le point A et B en fonction de t
+            t += Time.deltaTime * vitesse; // Incrementation de t selon le temps écoulé
+            if (t > 1f) t = 1f; // Limite t à 1 pour ne pas dépasser la destination
 
-            // Réinitialiser t pour recommencer le mouvement
-            t = 0f;
-        }
+            // Déplacer la plateforme
+            transform.position = Vector3.Lerp(pointA, pointB, t);
+
+
+            // Revenir au point A une fois arrivé à B
+            if (t >= 1f)
+            {
+                t = 0f;
+                StopForASec = false;
+                StartCoroutine(MovePlat());
+                // Inverser les points pour faire revenir la plateforme au point A
+                Vector3 temp = pointA;
+                pointA = pointB;
+                pointB = temp;
+            }
+        }        
+    }
+
+    IEnumerator MovePlat()
+    {
+        yield return new WaitForSeconds(1);
+        StopForASec = true;
     }
 }
