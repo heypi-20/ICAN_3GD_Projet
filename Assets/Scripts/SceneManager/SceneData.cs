a779f3647cc806c0269e2d673a8def8bc1605788
KiraYam <loicding93500@hotmail.com>
2024-11-18 15:59:53 +0100

    [add] multiple scene manager (not complete)

---
+++ Assets/Scripts/SceneManager/SceneData.cs
@@ -0,0 +1,11 @@
+﻿using Eflatun.SceneReference;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SceneData", menuName = "SceneData", order = 0)]
+public class SceneData : ScriptableObject
+{
+    public SceneReference sceneRef;
+    public bool isPersistant;
+    public bool isOpen;
+}
+
