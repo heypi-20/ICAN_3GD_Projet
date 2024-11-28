42ce8670eb9a3ed3d7fcedbf2af80ef5b5682afa
Sp4tulle <118128312+Sp4tulle@users.noreply.github.com>
2024-11-28 16:24:22 +0100

    Upgrade Bounce

--- Assets/PersonalFolders_Leo/Scripts/Trampoline.cs
+++ Assets/PersonalFolders_Leo/Scripts/Trampoline.cs
@@ -1,11 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Trampoline : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    public Transform _replacePoint;
+    public float _forceBounce;
+    public float _growthFactor;
+    public float _scaleMax = 2f;
+    private float _forceScale = 0f;
+    public bool _forceEqualScale = false;
+
+    void Start()
     {
-        collision.collider.GetComponent<Rigidbody>().AddForce(Vector3.up * 20, ForceMode.Impulse);
+        
+    }
+    
+    private void Update()
+    {
+        if (gameObject.transform.localScale.magnitude <= _scaleMax)
+        {
+            gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x * _growthFactor , gameObject.transform.localScale.y * _growthFactor, gameObject.transform.localScale.z * _growthFactor);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        other.transform.position = _replacePoint.position;
+        other.rigidbody.AddForce(Vector3.up * _forceBounce, ForceMode.Impulse);
     }
 }
