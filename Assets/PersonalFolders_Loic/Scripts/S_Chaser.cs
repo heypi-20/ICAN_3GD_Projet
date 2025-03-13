a48c415be599cc7f8a53f009d144a60c9b01c406
KiraYam <loicding93500@hotmail.com>
2025-03-13 19:00:48 +0100

    [add] chaser behavior and navmeshagent

---
+++ Assets/PersonalFolders_Loic/Scripts/S_Chaser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Rigidbody))]
+public class S_Chaser : EnemyBase
+{
+    [Header("Enemy Properties")]
+    public float maxChaseDistance = 5f;
+    
+    private NavMeshAgent agent;
+
+    private Transform target;
+    private S_CustomCharacterController findPlayer;
+    
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        findPlayer = FindObjectOfType<S_CustomCharacterController>();
+        if (findPlayer == null) {
+            Debug.LogWarning("No Character Controller");
+        }
+    }
+
+    private void Update()
+    {
+        target = findPlayer.transform;
+
+        if (Vector3.Distance(target.position, transform.position) < maxChaseDistance) {
+            agent.SetDestination(target.position);
+        }
+    }
+}
