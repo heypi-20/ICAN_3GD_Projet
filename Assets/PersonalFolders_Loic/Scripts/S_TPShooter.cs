cc3800e7088505f93e1ea97c974d00f97ca565dd
KiraYam <loicding93500@hotmail.com>
2025-03-21 16:10:03 +0100

    [add] new tpshooter version with navmesh and laser shots

--- Assets/PersonalFolders_Loic/Scripts/S_TPShooter.cs
+++ Assets/PersonalFolders_Loic/Scripts/S_TPShooter.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using UnityEngine.AI;
@@ -14,20 +16,24 @@ public class S_TPShooter : EnemyBase
     public float maxDist = 5f;
 
     [Header("Shoot Properties")]
-    public float fireRate;
     public float range;
-    public Transform projectilePrefab;
     public Transform shootPoint;
-    public float projectileSpeed = 10f;
+    public float shootDelay;
     
     [Header("Enemy Components")]
     private NavMeshAgent agent;
+    private S_CustomCharacterController findPlayer;
     private Transform player;
     private LineRenderer lr;
     private RaycastHit hit;
 
     private float teleportTimer;
     private float shootTimer;
+    private float lerpTimer;
+    
+    private bool canShoot = true;
+    private bool isCharging;
+    private bool laserCharged = false;
 
     private void Awake()
     {
@@ -36,17 +42,23 @@ public class S_TPShooter : EnemyBase
 
     private void Start()
     {
-        player = FindObjectOfType<S_CustomCharacterController>().transform;
+        findPlayer = FindObjectOfType<S_CustomCharacterController>();
+        if (findPlayer == null) {
+            Debug.LogWarning("No Character Controller");
+        }
+        
         lr = GetComponent<LineRenderer>();
-        lr.startColor = Color.red;
-        lr.endColor = Color.red;
+        
+        lr.startColor = new Color(1f, 0.36f, 0f);
+        lr.endColor = new Color(1f, 0.36f, 0f);
+        lr.startWidth = 0.05f;
+        lr.endWidth = 0.05f;
         lr.SetPosition(1, shootPoint.position);
     }
     
     void Update()
     {
-        if (player == null)
-            return;
+        player = findPlayer.transform;
         
         lr.SetPosition(0, shootPoint.position);
         
@@ -54,22 +66,17 @@ public class S_TPShooter : EnemyBase
 
         if (teleportTimer >= teleportCd) {
             Teleport();
+            canShoot = true;
             teleportTimer = 0;
         }
-        
-        shootTimer += Time.deltaTime;
         
         float dist = Vector3.Distance(transform.position, player.position);
 
         if (dist < range) {
-            
-            if (shootTimer >= fireRate) {
-                Shoot();
-                shootTimer = 0;
-            } else {
-                transform.LookAt(player.position);
-                LaserAim();
-            }
+            transform.LookAt(player.position);
+            LaserAim();
+        } else {
+            lr.SetPosition(1, shootPoint.position);
         }
     }
 
@@ -79,7 +86,7 @@ public class S_TPShooter : EnemyBase
         float randY = Random.Range(-minDist, maxDist);
         float randZ = Random.Range(-minDist, maxDist);
         
-        Vector3 targetPosition = new Vector3(transform.position.x + randX, transform.position.y, transform.position.z + randZ);
+        Vector3 targetPosition = new Vector3(transform.position.x + randX, transform.position.y + randY, transform.position.z + randZ);
         NavMeshHit navMeshHit;
         if (NavMesh.SamplePosition(targetPosition, out navMeshHit, range, NavMesh.AllAreas)) {
             agent.enabled = false;
@@ -103,19 +110,46 @@ public class S_TPShooter : EnemyBase
     {
         if (Physics.Raycast(transform.position, transform.forward, out hit, range)) {
             if (hit.collider) {
-                lr.SetPosition(1, hit.point);
+                if (!laserCharged) {
+                    lr.SetPosition(1, hit.point);
+                    lr.endWidth = Mathf.Min(lr.endWidth + 0.001f, 0.2f);
+                    
+                    lerpTimer += Time.deltaTime;
+                    Color chargedColor = Color.red;
+                    Color lerpColor = Color.Lerp(new Color(1f, 0.36f, 0f), chargedColor, Mathf.PingPong(lerpTimer, 1));
+                    lr.startColor = lerpColor;
+                    lr.endColor = lerpColor;
+                }
+                if (!canShoot)
+                    return;
+                if (!isCharging) {
+                    isCharging = true;
+                    StartCoroutine(Shoot(shootDelay));
+                }
             }
+        } else {
+            lr.SetPosition(1, transform.position + transform.forward * range);
         }
     }
 
-    private void Shoot()
+    IEnumerator Shoot(float delay)
     {
+        yield return new WaitForSeconds(delay);
+        laserCharged = true;
+        yield return new WaitForSeconds(delay/2f);
+        
         if (Physics.Raycast(transform.position, transform.forward, out hit, range)) {
-            if (hit.transform == player) {
-                Transform projectile = Instantiate(projectilePrefab, shootPoint.position, transform.rotation);
-                projectile.GetComponent<S_ProjectileSpeed>().speed = projectileSpeed;
+            if (hit.collider && hit.collider.CompareTag("Player")) {
+                hit.collider.GetComponent<S_EnergyStorage>().RemoveEnergy(enemyDamage);
             }
         }
+        
+        lr.startColor = new Color(1f, 0.36f, 0f);
+        lr.endColor = new Color(1f, 0.36f, 0f);
+        lr.endWidth = 0.05f;
+        laserCharged = false;
+        isCharging = false;
+        lerpTimer = 0;
     }
 }
 
