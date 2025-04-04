3613dd77b74a130d21f24c56cf8eec34ca623cf3
ArthurLUNAIS <lunaisarthur@gmail.com>
2025-04-04 17:37:08 +0200

    grand ménage script feedback

--- Assets/Common/Scripts/Feedback/S_CameraFeedBack.cs
+++ Assets/Common/Scripts/Feedback/S_CameraFeedBack.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using Cinemachine;
 using UnityEngine;
 using DG.Tweening;
+using Random = UnityEngine.Random;
 
 public class S_CameraFeedBack : MonoBehaviour
 {
@@ -36,21 +37,24 @@ public class S_CameraFeedBack : MonoBehaviour
     private CinemachineImpulseSource _impulseSource;
     
     [Header("Pillonage Shake")]
-    public float CameraShakePillonage;
-    public float stopDuration = 0.2f;
-    public int normalFPS = 60;     // FPS normal du jeu
-    public int effectFPS = 24;     // FPS pendant l'effet (plus bas = plus "cinématique")
-    public float duration = 0.5f;  // Durée de l'effet en secondes
+    [Space(5)]
     private float _timer_of_groundpound;
+    public int _WobbleCountPillonage = 4;
+    public float _WobbleAngle = 10f;
+    public float _DuraionWobble = 0.05f;
+    public float _RecoverDurationWobble = 0.1f;
+    public float startimpulseshakeforce = 3f;
+    private float impulseforceonGround;
+    public float _imulseMultiplicator = 0f;
+    
+    [Header("ChangeFPS")]
+    public float stopDuration = 0.2f;
+    public int effectFPS = 24;     // FPS pendant l'effet (plus bas = plus "cinématique")
+    public float duration = 0.5f; 
     private bool isActive = false;
-    public float tiltAnglePillonage;
-    public float shakeIntensityPillonage;
-    public float fallDurationPillonage;
-    public float recoverDurationPillonage;
 
     [Header("Pillonage FOV")] 
     public float fov_multiplicator;
-
     public float fov_transition_time;
 
     private float Curent_FOV;
@@ -103,85 +107,147 @@ public class S_CameraFeedBack : MonoBehaviour
         if (isIncreasing)
         {
             timePassed += Time.deltaTime;
+            impulseforceonGround = startimpulseshakeforce + timePassed * _imulseMultiplicator;
             _cinemachineVirtualCamera.m_Lens.FieldOfView = Start_FOV + timePassed * fov_multiplicator;
         }
     }
 
-    private void EnableCamera(bool state)
-    {
-        if (_cinemachineInputProvider != null)
-            _cinemachineInputProvider.enabled = state;
-    }
+    #region CameraMooveFeedback
 
-    private void ReceiveMoveEvent(Enum movingState, Vector2 inputDirection)
-    {
-        if (HasDirectionChanged(inputDirection))
+     private void EnableCamera(bool state)
         {
-            CameraDutch(inputDirection);
+            if (_cinemachineInputProvider != null)
+                _cinemachineInputProvider.enabled = state;
         }
-        if (inputDirection.y > 0.1f)
-            CameraTilt(-maxPitchAngle);   
-        else if (inputDirection.y < -0.1f)
-            CameraTilt(maxPitchAngle);    
-        else
-            CameraTilt(0f);   
-    }
     
-    private void CameraTilt(float targetAngle)
-    {
-        if (_currentTiltCoroutine != null)
-            StopCoroutine(_currentTiltCoroutine);
-
-        _currentTiltCoroutine = StartCoroutine(ApplyTiltEffectWithReturn(targetAngle));
-    }
-
-    private IEnumerator ApplyTiltEffectWithReturn(float targetAngle)
-    {
-        float startAngle = _currentTiltAngle;
-        float elapsedTime = 0f;
-
-        // 1. 进入倾斜
-        while (elapsedTime < pitchDuration)
+        private void ReceiveMoveEvent(Enum movingState, Vector2 inputDirection)
         {
-            elapsedTime += Time.deltaTime;
-            float t = Mathf.Clamp01(elapsedTime / pitchDuration);
-            float curveT = pitchToAngleCurve.Evaluate(t);
-
-            _currentTiltAngle = Mathf.Lerp(startAngle, targetAngle, curveT);
-            if (_recomposer != null)
-                _recomposer.m_Tilt = _currentTiltAngle;
-
-            yield return null;
+            if (HasDirectionChanged(inputDirection))
+            {
+                CameraDutch(inputDirection);
+            }
+            if (inputDirection.y > 0.1f)
+                CameraTilt(-maxPitchAngle);   
+            else if (inputDirection.y < -0.1f)
+                CameraTilt(maxPitchAngle);    
+            else
+                CameraTilt(0f);   
         }
-
-        _currentTiltAngle = targetAngle;
         
-        if (!autoResetTilt)
-            yield break;
-        
-        yield return new WaitForSeconds(0.05f);
-        
-        startAngle = _currentTiltAngle;
-        elapsedTime = 0f;
-
-        while (elapsedTime < pitchDuration)
+        private void CameraTilt(float targetAngle)
         {
-            elapsedTime += Time.deltaTime;
-            float t = Mathf.Clamp01(elapsedTime / pitchDuration);
-            float curveT = pitchToZeroCurve.Evaluate(t);
-
-            _currentTiltAngle = Mathf.Lerp(startAngle, 0f, curveT);
-            if (_recomposer != null)
-                _recomposer.m_Tilt = _currentTiltAngle;
-
-            yield return null;
-        }
-
-        _currentTiltAngle = 0f;
-        if (_recomposer != null)
-            _recomposer.m_Tilt = 0f;
-    }
+            if (_currentTiltCoroutine != null)
+                StopCoroutine(_currentTiltCoroutine);
     
+            _currentTiltCoroutine = StartCoroutine(ApplyTiltEffectWithReturn(targetAngle));
+        }
+        private IEnumerator ApplyTiltEffectWithReturn(float targetAngle)
+        {
+            float startAngle = _currentTiltAngle;
+            float elapsedTime = 0f;
+
+            // 1. 进入倾斜
+            while (elapsedTime < pitchDuration)
+            {
+                elapsedTime += Time.deltaTime;
+                float t = Mathf.Clamp01(elapsedTime / pitchDuration);
+                float curveT = pitchToAngleCurve.Evaluate(t);
+
+                _currentTiltAngle = Mathf.Lerp(startAngle, targetAngle, curveT);
+                if (_recomposer != null)
+                    _recomposer.m_Tilt = _currentTiltAngle;
+
+                yield return null;
+            }
+
+            _currentTiltAngle = targetAngle;
+        
+            if (!autoResetTilt)
+                yield break;
+        
+            yield return new WaitForSeconds(0.05f);
+        
+            startAngle = _currentTiltAngle;
+            elapsedTime = 0f;
+
+            while (elapsedTime < pitchDuration)
+            {
+                elapsedTime += Time.deltaTime;
+                float t = Mathf.Clamp01(elapsedTime / pitchDuration);
+                float curveT = pitchToZeroCurve.Evaluate(t);
+
+                _currentTiltAngle = Mathf.Lerp(startAngle, 0f, curveT);
+                if (_recomposer != null)
+                    _recomposer.m_Tilt = _currentTiltAngle;
+
+                yield return null;
+            }
+
+            _currentTiltAngle = 0f;
+            if (_recomposer != null)
+                _recomposer.m_Tilt = 0f;
+        }
+        private bool HasDirectionChanged(Vector2 newDirection)
+        {
+            float threshold = 0.1f;
+
+            bool xDirectionChanged = (Mathf.Abs(newDirection.x) > threshold) != (Mathf.Abs(_previousInputDirection.x) > threshold);
+            bool yDirectionChanged = (Mathf.Abs(newDirection.y) > threshold) != (Mathf.Abs(_previousInputDirection.y) > threshold);
+
+            _previousInputDirection = newDirection;
+
+            return xDirectionChanged || yDirectionChanged;
+        }
+    
+        private void CameraDutch(Vector2 direction)
+        {
+            float targetDutch = 0f;
+
+            if (direction.x > 0.1f) targetDutch = -maxDutchAngle;
+            else if (direction.x < -0.1f) targetDutch = maxDutchAngle;
+            else if (direction.x == 0) targetDutch = 0f;
+
+            if (_currentDutchCoroutine != null)
+            {
+                StopCoroutine(_currentDutchCoroutine);
+            }
+
+            _currentDutchCoroutine = StartCoroutine(ApplyDutchEffect(targetDutch));
+        }
+
+        private IEnumerator ApplyDutchEffect(float targetAngle)
+        {
+            float startAngle = _currentDutchAngle;
+            float duration = (targetAngle == 0) ? resetDuration : dutchDuration;
+            float elapsedTime = 0f;
+
+            while (elapsedTime < duration)
+            {
+                elapsedTime += Time.deltaTime;
+                float t = elapsedTime / duration;
+                t = (targetAngle == 0) ? resetCurve.Evaluate(t) : dutchCurve.Evaluate(t);
+
+                _currentDutchAngle = Mathf.Lerp(startAngle, targetAngle, t);
+                _cinemachineVirtualCamera.m_Lens.Dutch = _currentDutchAngle;
+
+                yield return null;
+            }
+
+            _currentDutchAngle = targetAngle;
+            _cinemachineVirtualCamera.m_Lens.Dutch = _currentDutchAngle;
+        }
+        private void OnDisable()
+        {
+            if (S_PlayerStateObserver.Instance != null)
+            {
+                S_PlayerStateObserver.Instance.OnMoveStateEvent -= ReceiveMoveEvent;
+            }
+        }
+
+    #endregion
+
+    #region MeleeAttack
+
     private void ReceiveMeleeAttackEvent(Enum state, int level)
     {
         if (state.Equals(PlayerStates.MeleeState.MeleeAttackHit))
@@ -203,14 +269,28 @@ public class S_CameraFeedBack : MonoBehaviour
             }
         }
     }
+    public void CameraShake(float intensity = 1f)
+    {
+        if (_impulseSource != null)
+        {
+            _impulseSource.GenerateImpulse(Vector3.one * intensity);
+            //Debug.Log("Ca tape fooooooooooort");
+        }
+        else
+        {
+            //Debug.LogWarning("CinemachineImpulseSource is missing on this GameObject.");
+        }
+    }
+
+    #endregion
+
+    #region GroundPound
 
     private void ReceiveGroudPoundEvevent(Enum state)
     {
         if (state.Equals(PlayerStates.GroundPoundState.EndGroundPound))
         {
-            //CameraFallImpactEffect(tiltAnglePillonage,shakeIntensityPillonage,fallDurationPillonage,recoverDurationPillonage);
-            CameraWobbleEffect();
-            CameraDutchWobble();
+            CameraDutchWobbleWithImpulse(_WobbleCountPillonage,_WobbleAngle,_DuraionWobble,_RecoverDurationWobble,impulseforceonGround);
             StartCoroutine(TimeStopCoroutine());
             if (!isActive)
             {
@@ -235,77 +315,21 @@ public class S_CameraFeedBack : MonoBehaviour
             _timer_of_groundpound = 0f;
         }
     }
-    
-    private bool HasDirectionChanged(Vector2 newDirection)
-    {
-        float threshold = 0.1f;
-
-        bool xDirectionChanged = (Mathf.Abs(newDirection.x) > threshold) != (Mathf.Abs(_previousInputDirection.x) > threshold);
-        bool yDirectionChanged = (Mathf.Abs(newDirection.y) > threshold) != (Mathf.Abs(_previousInputDirection.y) > threshold);
-
-        _previousInputDirection = newDirection;
-
-        return xDirectionChanged || yDirectionChanged;
-    }
-
-    #region CameraDutch
-    private void CameraDutch(Vector2 direction)
-    {
-        float targetDutch = 0f;
-
-        if (direction.x > 0.1f) targetDutch = -maxDutchAngle;
-        else if (direction.x < -0.1f) targetDutch = maxDutchAngle;
-        else if (direction.x == 0) targetDutch = 0f;
-
-        if (_currentDutchCoroutine != null)
-        {
-            StopCoroutine(_currentDutchCoroutine);
-        }
-
-        _currentDutchCoroutine = StartCoroutine(ApplyDutchEffect(targetDutch));
-    }
-
-    private IEnumerator ApplyDutchEffect(float targetAngle)
-    {
-        float startAngle = _currentDutchAngle;
-        float duration = (targetAngle == 0) ? resetDuration : dutchDuration;
-        float elapsedTime = 0f;
-
-        while (elapsedTime < duration)
-        {
-            elapsedTime += Time.deltaTime;
-            float t = elapsedTime / duration;
-            t = (targetAngle == 0) ? resetCurve.Evaluate(t) : dutchCurve.Evaluate(t);
-
-            _currentDutchAngle = Mathf.Lerp(startAngle, targetAngle, t);
-            _cinemachineVirtualCamera.m_Lens.Dutch = _currentDutchAngle;
-
-            yield return null;
-        }
-
-        _currentDutchAngle = targetAngle;
-        _cinemachineVirtualCamera.m_Lens.Dutch = _currentDutchAngle;
-    }
-    #endregion
-    private void OnDisable()
-    {
-        if (S_PlayerStateObserver.Instance != null)
-        {
-            S_PlayerStateObserver.Instance.OnMoveStateEvent -= ReceiveMoveEvent;
-        }
-    }
-    public void CameraShake(float intensity = 1f)
+    public void CameraDutchWobbleWithImpulse(int wobbleCount, float wobbleAngle, float wobbleSpeed, float recoverDuration,
+        float impulseStrength)
     {
         if (_impulseSource != null)
         {
-            _impulseSource.GenerateImpulse(Vector3.one * intensity);
-            //Debug.Log("Ca tape fooooooooooort");
-        }
-        else
-        {
-            //Debug.LogWarning("CinemachineImpulseSource is missing on this GameObject.");
+            Vector3 impulseDirection = new Vector3(
+                Random.Range(-0.5f, 0.5f),
+                Random.Range(-0.5f, 0.5f),
+                Random.Range(-0.2f, 0.2f)
+            ).normalized;
+
+            _impulseSource.GenerateImpulse(impulseDirection * impulseStrength);
         }
     }
+    
     private IEnumerator TimeStopCoroutine()
     {
         // Arrêter le temps
@@ -318,123 +342,26 @@ public class S_CameraFeedBack : MonoBehaviour
         Time.timeScale = 1f;
     }
     private IEnumerator ChangeFPS()
-        {
-            isActive = true;
-            
-            
-            // Applique le FPS réduit
-            Application.targetFrameRate = effectFPS;
-            //Debug.Log("Baisse Fps");
-            
-            // Attend la durée spécifiée
-            yield return new WaitForSecondsRealtime(duration);
-            
-            // Rétablit le FPS normal
-            Application.targetFrameRate = -1;
-            //Debug.Log("Débloooouér sa mèèèèèèère");
-            
-            isActive = false;
-        }
-
-    public void CameraWobbleEffect(int wobbleCount = 4, float wobbleAngle = 6f, float wobbleSpeed = 0.05f, float recoverDuration = 0.1f)
     {
-        if (_currentTiltCoroutine != null)
-            StopCoroutine(_currentTiltCoroutine);
-
-        _currentTiltCoroutine = StartCoroutine(WobbleCoroutine(wobbleCount, wobbleAngle, wobbleSpeed, recoverDuration));
+        isActive = true;
+            
+            
+        // Applique le FPS réduit
+        Application.targetFrameRate = effectFPS;
+        //Debug.Log("Baisse Fps");
+            
+        // Attend la durée spécifiée
+        yield return new WaitForSecondsRealtime(duration);
+            
+        // Rétablit le FPS normal
+        Application.targetFrameRate = -1;
+        //Debug.Log("Débloooouér sa mèèèèèèère");
+            
+        isActive = false;
     }
 
-    private IEnumerator WobbleCoroutine(int count, float angle, float speed, float recoverDuration)
-    {
-        float elapsed = 0f;
-
-        for (int i = 0; i < count; i++)
-        {
-            float target = (i % 2 == 0) ? angle : -angle;
-            float start = _recomposer != null ? _recomposer.m_Tilt : 0f;
-            elapsed = 0f;
-
-            while (elapsed < speed)
-            {
-                elapsed += Time.deltaTime;
-                float t = elapsed / speed;
-                float tilt = Mathf.Lerp(start, target, pitchToAngleCurve.Evaluate(t));
-
-                if (_recomposer != null)
-                    _recomposer.m_Tilt = tilt;
-
-                yield return null;
-            }
-        }
-
-        // Retour progressif à 0
-        float endAngle = _recomposer.m_Tilt;
-        elapsed = 0f;
-
-        while (elapsed < recoverDuration)
-        {
-            elapsed += Time.deltaTime;
-            float t = elapsed / recoverDuration;
-            float tilt = Mathf.Lerp(endAngle, 0f, pitchToZeroCurve.Evaluate(t));
-
-            if (_recomposer != null)
-                _recomposer.m_Tilt = tilt;
-
-            yield return null;
-        }
-
-        _currentTiltAngle = 0f;
-    }
-    public void CameraDutchWobble(int wobbleCount = 4, float wobbleAngle = 6f, float wobbleSpeed = 0.05f, float recoverDuration = 0.1f)
-    {
-        if (_currentDutchCoroutine != null)
-            StopCoroutine(_currentDutchCoroutine);
-
-        _currentDutchCoroutine = StartCoroutine(DutchWobbleCoroutine(wobbleCount, wobbleAngle, wobbleSpeed, recoverDuration));
-    }
-
-    private IEnumerator DutchWobbleCoroutine(int count, float angle, float speed, float recoverDuration)
-    {
-        float elapsed = 0f;
-
-        for (int i = 0; i < count; i++)
-        {
-            float target = (i % 2 == 0) ? angle : -angle;
-            float start = _cinemachineVirtualCamera.m_Lens.Dutch;
-
-            // Démarre directement l’effet visuel pour la première frame
-            _cinemachineVirtualCamera.m_Lens.Dutch = target;
-            yield return null;
-
-            elapsed = 0f;
-
-            while (elapsed < speed)
-            {
-                elapsed += Time.deltaTime;
-                float t = elapsed / speed;
-                float dutch = Mathf.Lerp(target, -target, dutchCurve.Evaluate(t)); // oscille à partir de target
-
-                _cinemachineVirtualCamera.m_Lens.Dutch = dutch;
-
-                yield return null;
-            }
-        }
-
-        // Retour à 0 smooth
-        float endDutch = _cinemachineVirtualCamera.m_Lens.Dutch;
-        elapsed = 0f;
-
-        while (elapsed < recoverDuration)
-        {
-            elapsed += Time.deltaTime;
-            float t = elapsed / recoverDuration;
-            float dutch = Mathf.Lerp(endDutch, 0f, resetCurve.Evaluate(t));
-
-            _cinemachineVirtualCamera.m_Lens.Dutch = dutch;
-
-            yield return null;
-        }
-
-        _currentDutchAngle = 0f;
-    }
+    #endregion
+    
 }
+
+    
--- Assets/Common/Scripts/Feedback/S_ExplosionFeedBack.cs
+++ Assets/Common/Scripts/Feedback/S_ExplosionFeedBack.cs
@@ -4,25 +4,10 @@ using System.Collections;
 
 public class ExplosionEffect : MonoBehaviour
 {
-    [Header("GroundPoundExplosion")]
-    public GameObject explosionPrefab;  // Le prefab de l'explosion
-    private GameObject currentExplosion; // Stocke l'objet instancié
+    [Header("Onde_GroundPound")]
 
-    public AnimationCurve sizeCurve;
-    public AnimationCurve alphaCurve;
-    public float duration = 1f;
-    public MeshRenderer sphereRenderer;  // Renseigner depuis l'inspecteur
-
-    private Material sphereMaterial;
-    public GameObject Explosion_GroundPound_SpawnPoint;
     
     [Header("ShockWave")]
-    //public Material shockwaveMaterial;
-    public float shockwaveSpeed = 10f;
-    public float maxDistance = 20f;
-    public float intensity = 10f;
-    public ParticleSystem ExplosionParticule;
-    private float shape_responsive;
 
     private float currentDistance = 0f;
     private bool isPlaying = false;
@@ -37,79 +22,21 @@ public class ExplosionEffect : MonoBehaviour
             S_PlayerStateObserver.Instance.OnGroundPoundStateEvent += ReceiceGroudPoundEvevent;
         }
     }
-
-    private void Update()
-    {
-        if (isPlaying)
-        {
-            currentDistance += Time.deltaTime * shockwaveSpeed;
-
-            // Envoie les valeurs au shader
-            shockwaveMat.SetVector("_Shockwave_Position", ShockWavePoint.transform.position);
-            shockwaveMat.SetFloat("_Shockwave_Distance", currentDistance);
-            shockwaveMat.SetFloat("_Shockwave_Intensity", Mathf.Lerp(intensity, 0f, currentDistance / maxDistance));
-
-            // Stopper une fois terminé
-            if (currentDistance >= maxDistance)
-            {
-                isPlaying = false;
-            }
-        }
-    }
-
-    private IEnumerator WaitForObserver()
-    {
-        float timeout = 3f;
-        float elapsedTime = 0f;
-
-        while (S_PlayerStateObserver.Instance == null)
-        {
-            if (elapsedTime >= timeout)
-            {
-                //Debug.LogError("S_PlayerStateObserver.Instance not found after waiting " + timeout + " seconds.");
-                yield break;
-            }
-
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
-
-        S_PlayerStateObserver.Instance.OnGroundPoundStateEvent += ReceiceGroudPoundEvevent;
-    }
+    
 
     private void ReceiceGroudPoundEvevent(Enum state)
     {
         if (state.Equals(PlayerStates.GroundPoundState.EndGroundPound))
         {
-            SpawnParticule(Explosion_GroundPound_SpawnPoint.transform.position);
+            SpawnExplosion_GroundPound(ShockWavePoint.transform.position);
         }
     }
-    private void SpawnParticule(Vector3 impactPosition)
+    private void SpawnExplosion_GroundPound(Vector3 impactPosition)
     {
-        S_GroundPound_Module groundPoundModule = FindObjectOfType<S_GroundPound_Module>();
-        float range = groundPoundModule.DynamicSphereRange;
-        
-        var main = ExplosionParticule.main;
-        main.startSize = range * 4f; // Exemple : vitesse proportionnelle au range
-        
-        ParticleSystem newParticles = Instantiate(ExplosionParticule, impactPosition, ExplosionParticule.transform.rotation);
-        
         float currentTime = Time.time;
-        shockwaveMat.SetFloat("_StartTime", currentTime);
         GameObject ShockWave = Instantiate(ImpactShockWave, impactPosition, ImpactShockWave.transform.rotation);
-        newParticles.Play();
         Destroy(ShockWave, 0.5f);
-        Destroy(newParticles.gameObject, newParticles.main.duration); // Nettoie après la durée de l'effet
     }
     
-    public void Play(Vector3 origin)
-    {
-        currentDistance = 0f;
-        isPlaying = true;
-        ShockWavePoint.transform.position = origin;
-
-        // Optionnel : si le shader a un paramètre "Enabled"
-        shockwaveMat.SetFloat("_Shockwave_Enabled", 1f);
-        shockwaveMat.SetFloat("_Shockwave_Max_Distance", maxDistance);
-    }
+    
 }
