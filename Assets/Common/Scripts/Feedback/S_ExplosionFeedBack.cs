27d96d462c923005e21f200e06b3d4ca84247a90
ArthurLUNAIS <lunaisarthur@gmail.com>
2025-04-07 16:57:25 +0200

    Explosion Littlechangement

--- Assets/Common/Scripts/Feedback/S_ExplosionFeedBack.cs
+++ Assets/Common/Scripts/Feedback/S_ExplosionFeedBack.cs
@@ -11,6 +11,10 @@ public class ExplosionEffect : MonoBehaviour
 
     public GameObject ShockWavePoint;
     public GameObject ImpactShockWave;
+    private float distanceShockwave;
+    private bool In_Ground;
+    private Material myMaterial;
+    public float Shock_Wave_Speed_Multiplayer = 1f;
 
     private void Start()
     {
@@ -19,8 +23,18 @@ public class ExplosionEffect : MonoBehaviour
             S_PlayerStateObserver.Instance.OnGroundPoundStateEvent += ReceiceGroudPoundEvevent;
         }
     }
-    
 
+    private void Update()
+    {
+        if (In_Ground)
+        {
+            myMaterial = ImpactShockWave.GetComponent<Renderer>().sharedMaterial;
+            float dist = myMaterial.GetFloat("Dist");
+            Debug.Log("Valeur du shader Dist : " + dist);
+            dist = 2f;
+            dist = 2 + (Time.deltaTime * Shock_Wave_Speed_Multiplayer);
+        }
+    }
     private void ReceiceGroudPoundEvevent(Enum state)
     {
         if (state.Equals(PlayerStates.GroundPoundState.EndGroundPound))
@@ -30,13 +44,11 @@ public class ExplosionEffect : MonoBehaviour
     }
     private void SpawnExplosion_GroundPound(Vector3 impactPosition)
     {
-        float currentTime = Time.time;
         ParticleSystem Onde = Instantiate(OndeParticule, impactPosition, ImpactShockWave.transform.rotation);
         Onde.Play();
         GameObject ShockWave = Instantiate(ImpactShockWave, impactPosition, ImpactShockWave.transform.rotation);
+        In_Ground = true;
         Destroy(ShockWave, 0.5f);
         Destroy(Onde,2f);
     }
-    
-    
 }
