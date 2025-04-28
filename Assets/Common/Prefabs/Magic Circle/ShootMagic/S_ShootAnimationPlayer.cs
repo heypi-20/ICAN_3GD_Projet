a7c4b8154780d84ff5277829d37627adf15fb16b
MaitreJarek <124875159+MaitreJarek@users.noreply.github.com>
2025-04-28 15:55:38 +0200

    Anim Done

--- Assets/Common/Prefabs/Magic Circle/ShootMagic/S_ShootAnimationPlayer.cs
+++ Assets/Common/Prefabs/Magic Circle/ShootMagic/S_ShootAnimationPlayer.cs
@@ -7,9 +7,22 @@ public class S_ShootAnimationPlayer : MonoBehaviour
 {
     // Reference to the Animator component
     public Animator animator;
-    // Name of the Animator state to play
-    public string StartShootAnim;
-    
+    // Name of the Animator state to play "Start Shoot"
+    public string StartShootAnimPalier1;
+    public string StartShootAnimPalier2;
+    public string StartShootAnimPalier3;
+    public string StartShootAnimPalier4;
+    // Name of the Animator state to play "Stop Shoot"
+    public string StopShootAnimPalier1;
+    public string StopShootAnimPalier2;
+    public string StopShootAnimPalier3;
+    public string StopShootAnimPalier4;
+    // Name of the Animator state to play "Is Shooting"
+    public string IsShootingAnimPalier1;
+    public string IsShootingAnimPalier2;
+    public string IsShootingAnimPalier3;
+    public string IsShootingAnimPalier4;
+
     public void Start()
     {
         // Warn and exit if still null
@@ -18,29 +31,51 @@ public class S_ShootAnimationPlayer : MonoBehaviour
             Debug.LogWarning("S_AnimationPlayer: Animator reference not set or found on the GameObject.");
             return;
         }
-        // Warn and exit if stateName is empty
-        if (string.IsNullOrEmpty(StartShootAnim))
-        {
-            Debug.LogWarning("S_AnimationPlayer: stateName is null or empty.");
-            return;
-        }
 
         S_PlayerStateObserver.Instance.OnShootStateEvent += handleShootAnimation;
     }
-
     private void handleShootAnimation(Enum state, int level)
     {
         switch (state)
         {
-            case PlayerStates.ShootState.StartShoot:
-                PlayAnimation(StartShootAnim);
+            case PlayerStates.ShootState.StartShoot when level == 1:
+                PlayAnimation(StartShootAnimPalier1);
                 break;
-            //case PlayerStates.MeleeState.MeleeAttackHit:
-            //    PlayAnimation(PunchHitAnim);
-            //    break;
-            //case PlayerStates.MeleeState.MeleeAttackMissed:
-            //    PlayAnimation(PunchMissAnim);
-            //    break;
+            case PlayerStates.ShootState.StartShoot when level == 2:
+                PlayAnimation(StartShootAnimPalier2);
+                break;
+            case PlayerStates.ShootState.StartShoot when level == 3:
+                PlayAnimation(StartShootAnimPalier3);
+                break;
+            case PlayerStates.ShootState.StartShoot when level == 4:
+                PlayAnimation(StartShootAnimPalier4);
+                break;
+
+            case PlayerStates.ShootState.StopShoot when level == 1:
+                PlayAnimation(StopShootAnimPalier1);
+                break;
+            case PlayerStates.ShootState.StopShoot when level == 2:
+                PlayAnimation(StopShootAnimPalier2);
+                break;
+            case PlayerStates.ShootState.StopShoot when level == 3:
+                PlayAnimation(StopShootAnimPalier3);
+                break;
+            case PlayerStates.ShootState.StopShoot when level == 4:
+                PlayAnimation(StopShootAnimPalier4);
+                break;
+
+            case PlayerStates.ShootState.IsShooting when level == 1:
+                PlayAnimation(IsShootingAnimPalier1);
+                break;
+            case PlayerStates.ShootState.IsShooting when level == 2:
+                PlayAnimation(IsShootingAnimPalier2);
+                break;
+            case PlayerStates.ShootState.IsShooting when level == 3:
+                PlayAnimation(IsShootingAnimPalier3);
+                break;
+            case PlayerStates.ShootState.IsShooting when level == 4:
+                PlayAnimation(IsShootingAnimPalier4);
+                break;
         }
     }
 
