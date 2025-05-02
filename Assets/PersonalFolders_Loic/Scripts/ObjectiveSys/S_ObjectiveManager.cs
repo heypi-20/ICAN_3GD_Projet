e22584545e31d91d9096fd5ecca9dabb30a0cdaa
KiraYam <loicding93500@hotmail.com>
2025-05-02 11:09:29 +0200

    [add] objective system base, some test object for the objectives and a new scene for advanced testing

---
+++ Assets/PersonalFolders_Loic/Scripts/ObjectiveSys/S_ObjectiveManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_ObjectiveManager
+{
+    public Action<Objective> OnObjectiveAdded;
+    
+    public List<Objective> objectives { get; } = new();
+    private readonly Dictionary<string, List<Objective>> _objectiveMap = new();
+
+    // Adds an objective to the objective manager.
+    // If the objective has an EventTrigger, it's progress will be incremented
+    // by AddProgress when the event is triggered. Multiple objectives can have
+    // the same EventTrigger (i.e. MobKilled, ItemCollected, etc)
+    public void AddObjective(Objective objective)
+    {
+        objectives.Add(objective);
+
+        if (!string.IsNullOrEmpty(objective.EventTrigger)) {
+            if (!_objectiveMap.ContainsKey(objective.EventTrigger)) {
+                _objectiveMap.Add(objective.EventTrigger, new List<Objective>());
+            }
+            _objectiveMap[objective.EventTrigger].Add(objective);
+        }
+        OnObjectiveAdded?.Invoke(objective);
+    }
+
+    public void AddProgress(string eventTrigger, int value)
+    {
+        if (!_objectiveMap.ContainsKey(eventTrigger)) {
+            return;
+        }
+
+        foreach (Objective objective in _objectiveMap[eventTrigger]) {
+            objective.AddProgress(value);
+        }
+    }
+}
---
+++ Assets/PersonalFolders_Loic/Scripts/ObjectiveSys/TestObjective.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+public class TestObjective : MonoBehaviour
+{
+    private S_ObjectiveManager objManager;
+
+    private Objective killObjective;
+    private Objective killCuby;
+    
+    private S_ObjectiveDisplay objectiveDisplay;
+    
+    private void Start()
+    {
+        objManager = new S_ObjectiveManager();
+        killObjective = new Objective("KillEnemy", "Killed : {0}/{1}", 10);
+        killCuby = new Objective("KillEnemy", "Killed Cuby : {0}/{1}", 10);
+        objManager.AddObjective(killObjective);
+
+        objectiveDisplay = GetComponent<S_ObjectiveDisplay>();
+        objectiveDisplay.Init(killObjective);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K)) {
+            killObjective.AddProgress(1);
+            killCuby.AddProgress(1);
+        }
+    }
+}
+
