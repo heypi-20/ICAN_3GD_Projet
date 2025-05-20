7f7186d0542ec27f16efc31965ced9ed529a7957
KiraYam <loicding93500@hotmail.com>
2025-05-20 17:17:40 +0200

    [add] score code logic

---
+++ Assets/Common/Scripts/Score/S_ScoreManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class ScoreData
+{
+    public EnemyType enemyType;
+    public int scoreGiven;
+    [HideInInspector] public int killed = 0;
+}
+
+public class S_ScoreManager : MonoBehaviour
+{
+    public List<ScoreData> scoreDatas;
+    
+    [Header("Read Only Global Score")]
+    public int score;
+
+    private void OnEnable()
+    {
+        EnemyBase.OnEnemyKilled += AddScore;
+    }
+
+    private void OnDisable()
+    {
+        EnemyBase.OnEnemyKilled -= AddScore;
+    }
+
+    private void AddScore(EnemyType enemyType)
+    {
+        foreach (ScoreData scoreData in scoreDatas) {
+            if (enemyType == scoreData.enemyType) {
+                score += scoreData.scoreGiven;
+                scoreData.killed++;
+                Debug.Log("Current Killed " +  scoreData.enemyType + " : " + scoreData.killed);
+                break;
+            }
+        }
+        
+        Debug.Log("Actual Score : " + score);
+    }
+}
+
