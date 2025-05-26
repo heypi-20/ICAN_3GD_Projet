29cc1530eadb81c31b7179c86eb9ccde1ea2f8ba
KiraYam <loicding93500@hotmail.com>
2025-05-26 14:30:47 +0200

    [add] energy setter for tutorials

---
+++ Assets/Common/Scripts/Tutorial/S_TutorialEnergy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class S_TutorialEnergy : MonoBehaviour
+{
+    public float energy;
+
+    private S_EnergyStorage eStorage;
+
+    private void Start()
+    {
+        eStorage = FindObjectOfType<S_EnergyStorage>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && eStorage.currentEnergy != energy) {
+            eStorage.currentEnergy = energy;
+        }
+    }
+}
+
