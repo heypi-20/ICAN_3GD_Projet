768d58eacb8eab49250417a1649a7d44961c53ee
KiraYam <loicding93500@hotmail.com>
2025-05-26 11:15:48 +0200

    [add] script for tuto1

---
+++ Assets/PersonalFolders_Loic/Scripts/S_TutoCheck1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+
+public class S_TutoCheck1 : MonoBehaviour
+{
+    public int inputCount = 5;
+    public int jumpCount = 5;
+    
+    private int icount;
+    private int jcount;
+    private bool movementComplete;
+    private bool jumpComplete;
+
+    private bool once = false;
+    
+    private void OnEnable()
+    {
+        FindObjectOfType<S_CustomCharacterController>().OnMoveStateChange += CheckForMovement;
+        FindObjectOfType<S_SuperJump_Module>().OnJumpStateChange += CheckForJump;
+    }
+
+    private void OnDisable()
+    {
+        FindObjectOfType<S_CustomCharacterController>().OnMoveStateChange += CheckForMovement;
+        FindObjectOfType<S_SuperJump_Module>().OnJumpStateChange += CheckForJump;
+    }
+
+    private void Update()
+    {
+        if (movementComplete && jumpComplete && !once) {
+            Debug.Log("Tuto Complete");
+            once = true;
+        }
+    }
+
+    private void CheckForMovement(Enum playerState, Vector2 direction)
+    {
+        if (playerState.Equals(PlayerStates.MoveState.IsMoving)) {
+            icount++;
+        }
+
+        if (icount == inputCount) {
+            movementComplete = true;
+        }
+    }
+
+    private void CheckForJump(Enum playerState, int level)
+    {
+        if (playerState.Equals(PlayerStates.JumpState.Jump)) {
+            jcount++;
+        }
+
+        if (jcount == jumpCount) {
+            jumpComplete = true;
+        }
+    }
+}
+
