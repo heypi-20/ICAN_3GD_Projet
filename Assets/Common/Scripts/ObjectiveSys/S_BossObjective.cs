68f52cb76d1556a1f02952f4449aba831e153c00
KiraYam <loicding93500@hotmail.com>
2025-05-26 12:16:09 +0200

    [add] boss kill objective

---
+++ Assets/Common/Scripts/ObjectiveSys/S_BossObjective.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+public class S_BossObjective : MonoBehaviour
+{
+    [Header("Objective Properties")]
+    public ObjectiveParams objectiveParams;
+
+    private S_ObjectiveManager ObjectiveManager;
+    private Objective bossObjective;
+    
+    private bool bansheeBossKilled = false;
+    private bool cubyBossKilled = false;
+    private bool tpShooterBossKilled = false;
+
+    private void Start()
+    {
+        ObjectiveManager = GetComponent<S_ObjectiveSystem>().ObjectiveManager;
+
+        bossObjective = new Objective(
+            objectiveParams.eventTrigger,
+            objectiveParams.eventText,
+            objectiveParams.currentValue,
+            objectiveParams.maxValue
+        );
+        
+        ObjectiveManager.AddObjective(bossObjective);
+        EnemyBase.OnEnemyKilled += CheckBosses;
+    }
+
+    private void CheckBosses(EnemyType enemyType)
+    {
+        if (enemyType == EnemyType.Banshee_Boss) {
+            bossObjective.AddProgress(1);
+            bansheeBossKilled = true;
+        }
+        if (enemyType == EnemyType.Cuby_Boss) {
+            bossObjective.AddProgress(1);
+            cubyBossKilled = true;
+        }
+        if (enemyType == EnemyType.TpShooter_Boss) {
+            bossObjective.AddProgress(1);
+            tpShooterBossKilled = true;
+        }
+    }
+}
+
